6ea3fedcec1acdc46f5ba2b43f299901d1ebf31d
u013571833 <448517683@qq.com>
2017-01-16 22:01:10 +0800

    Initial Commit

---
+++ LocationManager-iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocationManager-iOS
+//
+//  Created by fazhao on 2017/1/16.
+//  Copyright © 2017年 fazhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
